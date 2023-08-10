f3f031e3d075ed4f8772da3d296e607c1b4469fc
DawidWilkowski <dawwilkowski123@gmail.com>
2023-08-10 14:05:54 +0200

    Views with sample movies

---
+++ moviesASPDotNet/Controllers/MovieViewsController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using moviesASPDotNet.Data;
+using moviesASPDotNet.Models;
+
+namespace moviesASPDotNet.Controllers
+{
+    [Controller]
+    public class MovieViewsController : Controller
+    {
+
+        private readonly MoviesDbContext _dbContext;
+        public MovieViewsController(MoviesDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        [HttpGet]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpGet]
+        [Route("/movies")]
+        public IActionResult MoviesPage()
+        {
+            List<Movie> movies = _dbContext.Movies.ToList();
+            return View(movies);
+        }
+    }
+}
