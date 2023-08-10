b033613a47c539893c1333c45bdaaba4f17edc06
DawidWilkowski <dawwilkowski123@gmail.com>
2023-08-10 10:53:51 +0200

    Added local database connection

---
+++ moviesASPDotNet/Migrations/20230810084522_Movies.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace moviesASPDotNet.Migrations
+{
+    /// <inheritdoc />
+    public partial class Movies : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Movies",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Length = table.Column<int>(type: "int", nullable: false),
+                    PathToImage = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    PathtoMovie = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Movies", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Movies");
+        }
+    }
+}
---
+++ moviesASPDotNet/Models/Movie.cs
@@ -0,0 +1,12 @@
+﻿namespace moviesASPDotNet.Models
+{
+    public class Movie
+    {
+        public int Id { get; set; }
+        public string Title { get; set; }
+        public int Length { get; set; }
+        public string PathToImage { get; set; }
+        public string Description { get; set; }
+        public string PathtoMovie { get; set; }
+    }
+}
